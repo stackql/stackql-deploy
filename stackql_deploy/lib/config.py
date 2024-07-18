c9a1436bee36e937edd858be413efa3accd1050b
Jeffrey Aven <javen@avensolutions.com>
2024-07-18 16:22:51 +1000

    1.5.9

--- stackql_deploy/lib/config.py
+++ stackql_deploy/lib/config.py
@@ -9,12 +9,6 @@ from jinja2 import TemplateError
 def from_json(value):
     return json.loads(value)
 
-# def to_json_string(value):
-#     return json.dumps(json.loads(value))
-
-# def remove_single_quotes(value):
-#     return str(value).replace("'", "")
-
 def base64_encode(value):
     return base64.b64encode(value.encode()).decode()
 
@@ -25,61 +19,71 @@ def merge_lists(tags1, tags2):
 
 # END jinja filters
 
+# def render_value(env, value, context):
+#     if isinstance(value, str):
+#         try:
+#             template = env.from_string(value)
+#             return template.render(**context)
+#         except TemplateError as e:
+#             print(f"Error rendering template: {e}")
+#             return value
+#     elif isinstance(value, dict):
+#         return {k: render_value(v, context) for k, v in value.items()}
+#     elif isinstance(value, list):
+#         return [render_value(item, context) for item in value]
+#     else:
+#         return value
+
+def render_value(env, value, context):
+    if isinstance(value, str):
+        try:
+            template = env.from_string(value)
+            rendered = template.render(**context)
+            if rendered in ['True', 'False']:
+                return rendered.replace('True', 'true').replace('False', 'false')
+            return rendered
+        except TemplateError as e:
+            print(f"Error rendering template: {e}")
+            return value
+    elif isinstance(value, dict):
+        return {k: render_value(env, v, context) for k, v in value.items()}
+    elif isinstance(value, list):
+        return [render_value(env, item, context) for item in value]
+    else:
+        return value
+
+# def render_globals(env, vars, global_vars, stack_env, stack_name):
+#     global_context = {'stack_env': stack_env, 'stack_name': stack_name}
+#     global_context.update(vars)
+
+#     for global_var in global_vars:
+#         global_context[global_var['name']] = render_value(env, global_var['value'], global_context)
+
+#     return global_context
+
 def render_globals(env, vars, global_vars, stack_env, stack_name):
     global_context = {'stack_env': stack_env, 'stack_name': stack_name}
     global_context.update(vars)
 
-    def render_value(value, context):
-        if isinstance(value, str):
-            try:
-                template = env.from_string(value)
-                return template.render(**context)
-            except TemplateError as e:
-                print(f"Error rendering template: {e}")
-                return value
-        elif isinstance(value, dict):
-            return {k: render_value(v, context) for k, v in value.items()}
-        elif isinstance(value, list):
-            return [render_value(item, context) for item in value]
-        else:
-            return value
-
     for global_var in global_vars:
-        global_context[global_var['name']] = render_value(global_var['value'], global_context)
+        rendered_value = render_value(env, global_var['value'], global_context)
+        if not rendered_value:
+            raise ValueError(f"Global variable '{global_var['name']}' cannot be empty.")
+        global_context[global_var['name']] = rendered_value
 
     return global_context
 
 def render_properties(env, resource_props, global_context, logger):
 
-    def render_value(value, context):
-        if isinstance(value, str):
-            try:
-                template = env.from_string(value)
-                # rendered = template.render(context)
-                rendered = template.render(**context)
-                # deal with boolean values
-                if rendered in ['True', 'False']:
-                    return rendered.replace('True', 'true').replace('False', 'false')
-                return rendered
-            except TemplateError as e:
-                print(f"Error rendering template: {e}")
-                return value
-        elif isinstance(value, dict):
-            return {k: render_value(v, context) for k, v in value.items()}
-        elif isinstance(value, list):
-            return [render_value(item, context) for item in value]
-        else:
-            return value
-   
     prop_context = {}
     for prop in resource_props:
         try:
             if 'value' in prop:
-                prop_context[prop['name']] = render_value(prop['value'], global_context)
+                prop_context[prop['name']] = render_value(env, prop['value'], global_context)
             elif 'values' in prop:
                 env_value = prop['values'].get(global_context['stack_env'], {}).get('value')
                 if env_value is not None:
-                    prop_context[prop['name']] = render_value(env_value, global_context)
+                    prop_context[prop['name']] = render_value(env, env_value, global_context)
                 else:
                     catch_error_and_exit(f"No value specified for property '{prop['name']}' in stack_env '{global_context['stack_env']}'.", logger)
         except Exception as e:
@@ -107,8 +111,6 @@ def setup_environment(stack_dir, logger):
         autoescape=False
     )
     env.filters['from_json'] = from_json
-    # env.filters['to_json_string'] = to_json_string
-    # env.filters['remove_single_quotes'] = remove_single_quotes
     env.filters['merge_lists'] = merge_lists
     env.filters['base64_encode'] = base64_encode
     return env
