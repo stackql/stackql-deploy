432e3780da2f98749b10ccf4351321e9fd5735f7
Jeffrey Aven <javen@avensolutions.com>
2024-09-17 09:26:48 +1000

    menu update

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import { Redirect } from '@docusaurus/router';
 
 export default function Home() {
-  return <Redirect to="/docs" />;
+  return (
+    <Head>
+    <meta http-equiv="refresh" content="0;URL='https://stackql.io/'" />
+    </Head>
+  );
 }
